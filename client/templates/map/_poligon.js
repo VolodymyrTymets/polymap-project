4fe99658a6b0710e559d0377f7bf38a1e062c0ea
vova tymets <volodymyrtymets@gmail.com>
2015-12-25 12:27:39 +0200

    add hover functional refactor code

--- client/templates/map/_poligon.js
+++ client/templates/map/_poligon.js
@@ -1,9 +1,46 @@
-var Circle = function (param) {
+Circle = function (param) {
     var _$element = param.$element;
     var _data = param.data;
     var _zoom = param.zoom;
+    var _R = 20 * Math.pow(2, _zoom - 12);
+
+    var _onMouseoverCircle  = function (e) {
+        $('text[data-name="'+$(e.target).attr('data-name')+'"]').attr('class','show');
+    }
+    var _onMouseleaveCircle  = function (e) {
+        $('text[data-name="'+$(e.target).attr('data-name')+'"]').attr('class','hide');
+    }
 
     this.init = function () {
+        var r =_R * _data.Frequency;
+        if(r > 2 && r < 200) {
+            var text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
 
+            $(_$element).attr('r', r);
+            $(text).text(_data.LocationName +' (Frequency:'+ _data.Frequency +')');
+            $(text).attr('transform', $(_$element).attr('transform'));
+
+            // Avoid overflow circle via parent element
+            $(_$element).parent().attr('clip-path','url(#org.polymaps.0)');
+            $(_$element).attr('clip-path','url(#org.polymaps.99)');
+
+            // Add class
+            $(text).attr("class", 'circle-label');
+            $(text).attr("class", 'hide');
+            $(_$element).attr("class", 'circle');
+            $(_$element).attr("data-name", _data.id);
+            $(text).attr("data-name", _data.id);
+
+            // Append events
+            $(_$element).mouseover(_onMouseoverCircle);
+            $(_$element).mouseleave(_onMouseleaveCircle);
+
+            // append element to parent
+            $(_$element).parent().append($(_$element));
+            $(_$element).parent().append(text);
+
+        }else{
+            $(_$element).remove()
+        }
     }
 }
--- client/templates/map/map.js
+++ client/templates/map/map.js
@@ -1,28 +1,5 @@
-//var _load = function (e,data) {
-//    debugger
-//    var cluster = e.tile.cluster || (e.tile.cluster = Template.Map._kmeans()
-//            .iterations(16)
-//            .size(64));
-//    Locations.find().forEach(function(location){
-//        if(location.LatLng) cluster.add(location.LatLng.split(','));
-//    })
-//
-//    var tile = e.tile, g = tile.element;
-//    while (g.lastChild) g.removeChild(g.lastChild);
-//
-//    var means = cluster.means();
-//    means.sort(function(a, b) { return b.size - a.size; });
-//    for (var i = 0; i < means.length; i++) {
-//        var mean = means[i], point = g.appendChild(Template.Map._Po.svg("circle"));
-//        point.setAttribute("cx", mean.x);
-//        point.setAttribute("cy", mean.y);
-//        point.setAttribute("r", Math.pow(2, tile.zoom - 11) * Math.sqrt(mean.size));
-//    }
-//}
-
 Template.Map.onRendered(function () {
-    // save poly maps
-    Template.Map._Po = org.polymaps;
+
     var po = org.polymaps;
 
     // Init map
@@ -32,47 +9,29 @@ Template.Map.onRendered(function () {
         .zoom(2)
         .add(po.interact());
 
+    // Get map from http://www.openstreetmap.org
     map.add(po.image()
         .url(po.url("http://{S}tile.openstreetmap.org"
         + "/{Z}/{X}/{Y}.png")
             .hosts(["a.", "b.", "c.", ""])));
 
-    //var data = [];
-    //Locations.find({},{sort: {Frequency: -1}, limit:100}).forEach(function(location){
-    //    if(location.LatLng) {
-    //        data.push({
-    //            geometry: {
-    //                coordinates: location.LatLng.split(',').reverse(),
-    //                type: "Point"
-    //            }
-    //        })
-    //    }
-    //});
-
+    // Load Data from server (lib/routes/locations.js)
     map.add(po.geoJson()
         .url("locations/{X}/{Y}/{Z}") // load from server
-       // .features(data)
         .on("load", load));
 
-    //map.add(po.compass()
-    //    .pan("none"));
 
+    // callback when data loaded
+    // (data will be reload if user change map zoom)
     function load(e) {
-        var r = 20 * Math.pow(2, e.tile.zoom - 12);
         for (var i = 0; i < e.features.length; i++) {
-          var c = $(e.features[i].element);
-          c.attr('r',r * e.features[i].data.Frequency);
-          c.attr("opacity", .5);
-          //var g = document.createElementNS('http://www.w3.org/2000/svg', 'g');
-          //var text =   document.createElementNS('http://www.w3.org/2000/svg', 'text');
-          //$(text).text( e.features[i].data.LocationName);
-          //$(text).attr('transform', c.attr('transform'))
-          //c.parent().add("svg:g", c);
-          c.parent().append(c);
-          //$(g).attr('transform', c.attr('transform'))
-          //$(g).append(c);
-          //$(g).append(text);
-
+          // Create new circle for each new data
+          var circle = new Circle({
+              $element:$(e.features[i].element),
+              data: e.features[i].data,
+              zoom:e.tile.zoom
+          });
+          circle.init();
         }
     }
-})
+});
